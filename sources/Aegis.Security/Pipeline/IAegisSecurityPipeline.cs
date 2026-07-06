0786bcdcaef5323a049bcf58f09f9850767edea5
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-07-06 12:23:34 +0200

    [Feat][Pipeline]: define iaegissecuritypipeline contract

---
+++ sources/Aegis.Security/Pipeline/IAegisSecurityPipeline.cs
@@ -0,0 +1,10 @@
+﻿using Aegis.Shared.Security.Models;
+
+namespace Aegis.Security.Pipeline;
+
+public interface IAegisSecurityPipeline
+{
+    Task<(AegisSecurityReport Report, SecurityComplianceResult Compliance)> RunAsync(
+        ProjectSecurityContext context,
+        CancellationToken ct = default);
+}

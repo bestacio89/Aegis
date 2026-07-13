40eaa6fa797a16bbc798497b917594fd2e357b64
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-07-13 14:12:31 +0200

    [Feat] [WPF] added scaffolding for workspace selection
    - added scaffolding for future  selection of workspaces

---
+++ sources/Aegis.App.WPF/ViewModels/Orchestrators/IWorkspaceOrchestrator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Aegis.Wpf.ViewModels.Orchestrators
+{
+    public interface IWorkspaceOrchestrator
+    {
+        string ModeName { get; }
+        IReadOnlyList<WorkspaceTabDescriptor> Tabs { get; }
+        object ActiveWorkspaceViewModel { get; }
+        ICommand SelectTabCommand { get; }
+
+        Task RunAnalysisAsync(string repositoryPath, CancellationToken token);
+        Task ExportAsync(string format, string outputPath, CancellationToken token);
+    }
+
+}
---
+++ sources/Aegis.App.WPF/ViewModels/Orchestrators/WorkspaceTabDescriptor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Aegis.Wpf.ViewModels.Orchestrators
+{
+    public sealed record WorkspaceTabDescriptor(
+      string Name, string Glyph, ICommand SelectCommand);
+}
